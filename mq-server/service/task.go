3f09217d3df8b0e2a311b6b6f72d3e9f55f2a192
ty322 <acchn@live.com>
2022-05-21 15:45:26 +0800

    feat: 完成mq-server部分

--- mq-server/service/task.go
+++ mq-server/service/task.go
@@ -1,6 +1,10 @@
 package service
 
-import "mq-server/model"
+import (
+	"encoding/json"
+	"log"
+	"mq-server/model"
+)
 
 // 从rabbitmq中接受信息, 写入数据库
 func CreateTask() {
@@ -9,4 +13,22 @@ func CreateTask() {
 		panic(err)
 	}
 	q, _ := ch.QueueDeclare("task_queue", true, false, false, false, nil)
+	ch.Qos(1, 0, false)
+	msgs, err := ch.Consume(q.Name, "", false, false, false, false, nil)
+	if err != nil {
+		panic(err)
+	}
+	// 处于一个监听状态, 一直监听我们的生产端的生产, 要阻塞主进程
+	go func() {
+		for d := range msgs {
+			var t model.Task
+			err := json.Unmarshal(d.Body, &t)
+			if err != nil {
+				panic(err)
+			}
+			model.DB.Create(&t)
+			log.Println("Done")
+			_ = d.Ack(false)
+		}
+	}()
 }
